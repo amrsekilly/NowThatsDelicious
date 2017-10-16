f0acbbc881cda1f279391875756a1871162ecbc1
Amr Elsekilly <amr.e.dev@gmail.com>
2017-10-16 18:44:20 +0200

    added a router method

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const userInfo = {"Name": "Amr", "Age": 25, "title": "Web Developer"};
+  // res.send('Hey! It works!');
+  res.json(userInfo);
+});
+
+router.get('/reverse/:name', (req, res) => {
+  // reverse the name entered in the prameters
+  let reversedName = [...req.params.name].reverse().join('');
+  res.send(reversedName);
 });
 
 module.exports = router;
