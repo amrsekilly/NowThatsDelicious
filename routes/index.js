a0cbe3dc7729b943401ba658a681c022f0e2e035
Amr Elsekilly <amr.e.dev@gmail.com>
2017-10-17 00:02:40 +0200

    passed in the page title into the render method

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,9 @@ router.get('/', (req, res) => {
   const userInfo = {"Name": "Amr", "Age": 25, "title": "Web Developer"};
   // res.send('Hey! It works!');
   // res.json(userInfo);
-   res.render("hello");
+   res.render("hello", {
+     title: "Welcome to the food club!"
+   });
 });
 
 router.get('/reverse/:name', (req, res) => {
